033781bf050ad1027ae111ea5a5cb8c4c7a0332a
zxwtry <zxwtry@qq.com>
2016-03-09 00:49:04 +0800

    简单的Java代码

--- src/jianZhiOffer/题36数组中的逆序对.java
+++ src/jianZhiOffer/题36数组中的逆序对.java
@@ -5,6 +5,48 @@ package jianZhiOffer;
  */
 public class 题36数组中的逆序对 {
 	public static void main(String[] args) {
-		
+		int[] data = {7, 5, 6, 4};
+		System.out.println(inversePairs(data, 0, data.length-1));
+	}
+	public static void mergeSortDivide (int[] data, int beginIndex, int endIndex) {
+		if (data == null || beginIndex >= endIndex)   return;
+		int mid = (beginIndex+endIndex) >> 1;
+		mergeSortDivide(data, beginIndex, mid);
+		mergeSortDivide(data, mid+1, endIndex);
+	}
+	public static int inversePairs (int[] data, int beginIndex, int endIndex) {
+		if (data == null || beginIndex > endIndex)   return 0;
+		int[] copy = new int[endIndex-beginIndex+1];
+		System.arraycopy(data, beginIndex, copy, 0, endIndex-beginIndex+1);
+		int count = inversePairs(data, copy, 0, copy.length-1);
+		return count;
+	}
+	private static int inversePairs (int[] data, int[] copy, int beginIndex, int endIndex) {
+		if (beginIndex == endIndex) {
+			copy[beginIndex] = data[beginIndex];
+			return 0;
+		}
+		int length = (endIndex - beginIndex) >> 1;
+		int left = inversePairs (copy, data, beginIndex, beginIndex+length);
+		int right = inversePairs (copy, data, beginIndex+length+1, endIndex);
+		//i初始化为前半段最后一个数字的下标
+		int i = beginIndex + length;
+		//j初始化为后半段最后一个数字的下标
+		int j = endIndex;
+		int indexCopy = endIndex;
+		int count = 0;
+		while (i >= beginIndex && j >= beginIndex+length+1) {
+			if (data[i] > data[j]) {
+				copy[indexCopy--] = data[i--];
+				count += j-beginIndex-length;
+			} else {
+				copy[indexCopy--] = data[j--];
+			}
+		}
+		for (; i >= beginIndex; -- i) 
+			copy[indexCopy--] = data[i];
+		for (; j >= beginIndex+length+1; --j)
+			copy[indexCopy--] = data[j];
+		return left+right+count;
 	}
 }

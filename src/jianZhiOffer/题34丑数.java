e5a452a727dd99e37f4aba5692362e368d5e92f3
zxwtry <zxwtry@qq.com>
2016-03-08 23:40:49 +0800

    简单的Java代码

--- src/jianZhiOffer/题34丑数.java
+++ src/jianZhiOffer/题34丑数.java
@@ -6,6 +6,24 @@ package jianZhiOffer;
  */
 public class Ìâ34³óÊı {
 	public static void main(String[] args) {
-		
+		System.out.println(getUglyNumber(8));
+	}
+	private static int getUglyNumber (int numth) {
+		int[] data;
+		if (numth < 6)
+			data = new int[6];
+		else
+			data = new int[numth];
+		data[0] = 1;   data[1] = 2;   data[2] = 3;
+		data[3] = 5;   data[4] = 6;   data[5] = 9;
+		int m2 = 3, m3 = 3, m5 = 1;
+		int index = 5;
+		while (index < numth-1) {
+			while (data[m2]*2 <= data[index])   m2 ++;
+			while (data[m3]*3 <= data[index])   m3 ++;
+			while (data[m5]*5 <= data[index])   m5 ++;
+			data[++index] = Math.min(Math.min(data[m2]*2, data[m3]*3),data[m5]*5);
+		}
+		return data[numth-1];
 	}
 }
---
+++ src/jianZhiOffer/题36数组中的逆序对.java
@@ -0,0 +1,10 @@
+package jianZhiOffer;
+/*
+ * 在数组中的两个数字如果前面一个数字大于后面的数字，则这两个数字组成一个逆序对。
+ * {7, 5, 6, 4}一共有5个逆序对，分别是(7,6) (7,5) (7,4) (6,4) (5,4)
+ */
+public class 题36数组中的逆序对 {
+	public static void main(String[] args) {
+		
+	}
+}

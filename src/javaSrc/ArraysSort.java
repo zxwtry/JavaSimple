da1e75e044bc51ed3dbc67032a34b69d170e160e
zxwtry <zxwtry@qq.com>
2016-03-10 23:53:06 +0800

    简单的Java代码

--- src/javaSrc/ArraysSort.java
+++ src/javaSrc/ArraysSort.java
@@ -1,6 +1,7 @@
 package javaSrc;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 public class ArraysSort {
 	public static void main(String[] args) {
@@ -11,6 +12,13 @@ public class ArraysSort {
 		for (int i = 0 ;i < b.length; i ++)
 			b[i] = new A(i+1);
 		tools.IOUtils.printAnArray(b);
+		A c = new A(100);
+		System.out.println("====");
+		System.out.println(c.toString());
+		System.out.println("====");
+		tools.IOUtils.printAnLongString(c.toString());
+		Arrays.sort(a, Arrays.re);
+		
 	}
 	static class A {
 		int[] data ;
@@ -22,9 +30,11 @@ public class ArraysSort {
 		@Override
 		public String toString () {
 			StringBuilder st = new StringBuilder ();
-			for (int i = 0; i < data.length; i ++) {
+			int i;
+			for (i = 0; i < data.length-1; i ++) {
 				st.append(data[i] + "\t");
 			}
+			st.append(data[i]+"\n");
 			return st.toString();
 		}
 	}
--- src/tools/IOUtils.java
+++ src/tools/IOUtils.java
@@ -3,6 +3,12 @@ package tools;
  * 这是自己写的一个用于读入输出的东东
  */
 public class IOUtils <T>{
+	public static void printAnNumber (int data) {
+		System.out.println(data);
+	}
+	public static <T> void printAnObject (T data) {
+		System.out.println(data.toString());
+	}
 	public static <T> void printAnArrayTenALine (T[] data, int beginIndex, int endIndex) {
 		int times = (endIndex-beginIndex)/10+1;
 		int timesIndex = 0;
@@ -16,26 +22,52 @@ public class IOUtils <T>{
 	}
 	public static <T> void printAnArray (T[] data, int beginIndex, int endIndex) {
 		for (int dataIndex = beginIndex; dataIndex <= endIndex; dataIndex ++) {
-			System.out.print(data[dataIndex].toString()+"\t");
+			System.out.print(data[dataIndex].toString());
 		}
 		System.out.println();
+	}
+	public static void printAnArray (short[] data) {
+		printAnArray(data, 0, data.length-1);
+	}
+	public static void printAnArray (short[] data, int beginIndex, int endIndex) {
+		for (int dataIndex = beginIndex; dataIndex < endIndex; dataIndex ++) {
+			System.out.printf("%d\t", data[dataIndex]);
+		}
+		System.out.println(data[endIndex]);
+	}
+	public static void printAnArray (char[] data) {
+		printAnArray(data, 0, data.length-1);
+	}
+	public static void printAnArray (char[] data, int beginIndex, int endIndex) {
+		for (int dataIndex = beginIndex; dataIndex < endIndex; dataIndex ++) {
+			System.out.print(data[dataIndex]+"\t");
+		}
+		System.out.println(data[endIndex]);
 	}
 	public static void printAnArray (int[] data) {
 		printAnArray(data, 0, data.length-1);
 	}
 	public static void printAnArray (int[] data, int beginIndex, int endIndex) {
-		for (int dataIndex = beginIndex; dataIndex <= endIndex; dataIndex ++) {
+		for (int dataIndex = beginIndex; dataIndex < endIndex; dataIndex ++) {
 			System.out.print(data[dataIndex]+"\t");
 		}
-		System.out.println();
+		System.out.println(data[endIndex]);
 	}
 	public static void printAnArray (double[] data) {
 		printAnArray(data, 0, data.length-1);
 	}
 	public static void printAnArray (double[] data, int beginIndex, int endIndex) {
-		for (int dataIndex = beginIndex; dataIndex <= endIndex; dataIndex ++) {
+		for (int dataIndex = beginIndex; dataIndex < endIndex; dataIndex ++) {
 			System.out.print(data[dataIndex]+"\t");
 		}
-		System.out.println();
+		System.out.println(data[endIndex]);
+	}
+	public static void printAnLongString (String str) {
+		int times = str.length()/20+1;
+		int timesIndex;
+		for (timesIndex = 0; timesIndex < times-1; timesIndex ++) {
+			System.out.println(str.substring(timesIndex*20, timesIndex*20+19));
+		}
+		System.out.println(str.substring(timesIndex*20, str.length()-1));
 	}
 }

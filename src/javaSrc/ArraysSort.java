31fed1bad0c657c2e9e0b8bfc409c7338ffc43f5
zxwtry <zxwtry@qq.com>
2016-03-10 23:26:33 +0800

    简单的Java代码

--- src/javaSrc/ArraysSort.java
+++ src/javaSrc/ArraysSort.java
@@ -7,5 +7,25 @@ public class ArraysSort {
 		int[] a = {2, 4, 6, 8, 1, 3, 5, 7 , 9, 0};
 		Arrays.sort(a);
 		tools.IOUtils.printAnArray(a);
+		A[] b = new A[10];
+		for (int i = 0 ;i < b.length; i ++)
+			b[i] = new A(i+1);
+		tools.IOUtils.printAnArray(b);
+	}
+	static class A {
+		int[] data ;
+		public A (int n) {
+			data = new int[n];
+			for (int i = 0 ; i < n ; i ++ ) 
+				data[i] = i*i;
+		}
+		@Override
+		public String toString () {
+			StringBuilder st = new StringBuilder ();
+			for (int i = 0; i < data.length; i ++) {
+				st.append(data[i] + "\t");
+			}
+			return st.toString();
+		}
 	}
 }
--- src/tools/IOUtils.java
+++ src/tools/IOUtils.java
@@ -5,9 +5,11 @@ package tools;
 public class IOUtils <T>{
 	public static <T> void printAnArrayTenALine (T[] data, int beginIndex, int endIndex) {
 		int times = (endIndex-beginIndex)/10+1;
-		for (int timesIndex = 0; timesIndex < times; timesIndex ++){
-			
+		int timesIndex = 0;
+		for (timesIndex = 0; timesIndex < times-1; timesIndex ++){
+			printAnArray(data, beginIndex+timesIndex*10, beginIndex+timesIndex*10+9);
 		}
+		printAnArray(data, beginIndex+timesIndex*10, endIndex);
 	}
 	public static <T> void printAnArray (T[] data) {
 		printAnArray(data, 0, data.length-1);
@@ -18,19 +20,19 @@ public class IOUtils <T>{
 		}
 		System.out.println();
 	}
-	public static <T> void printAnArray (int[] data) {
+	public static void printAnArray (int[] data) {
 		printAnArray(data, 0, data.length-1);
 	}
-	public static <T> void printAnArray (int[] data, int beginIndex, int endIndex) {
+	public static void printAnArray (int[] data, int beginIndex, int endIndex) {
 		for (int dataIndex = beginIndex; dataIndex <= endIndex; dataIndex ++) {
 			System.out.print(data[dataIndex]+"\t");
 		}
 		System.out.println();
 	}
-	public static <T> void printAnArray (double[] data) {
+	public static void printAnArray (double[] data) {
 		printAnArray(data, 0, data.length-1);
 	}
-	public static <T> void printAnArray (double[] data, int beginIndex, int endIndex) {
+	public static void printAnArray (double[] data, int beginIndex, int endIndex) {
 		for (int dataIndex = beginIndex; dataIndex <= endIndex; dataIndex ++) {
 			System.out.print(data[dataIndex]+"\t");
 		}
